b14b38bdf6ca60d4f000bf7187c4d324f5ec4c98
Lucy <lucy_oswald@hotmail.co.uk>
2023-05-11 18:16:34 +0100

    final commit

--- consts.js
+++ consts.js
@@ -1,1 +1,7 @@
-export const API_URL = "http://localhost:8000";
+// export const API_URL = "http://localhost:8000";
+// const DEV_API_URL = "http://localhost:8000";
+// const PROD_API_URL = process.env.REACT_APP_API_URL;
+// export const API_URL =
+//   process.env.NODE_ENV === "production" ? PROD_API_URL : DEV_API_URL;
+
+export const API_URL = "https://the-reuse-network.herokuapp.com";
